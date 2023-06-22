19be523466ee5037b1d9ff748e9827effe80c9ef
Collins Wakahenya <wakahenyacollins@gmail.com>
2023-06-22 17:49:21 +0300

    Done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+const mainElement = document.getElementById("main");
+mainElement.remove();
+
+// let newHeader = document.createElement("h1");
+// newHeader.id = "victory";
+
+const newHeader = document.createElement("h1");
+newHeader.id = "victory";
+newHeader.textContent = "Omosh is the champion";
